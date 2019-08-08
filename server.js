1a19cf891ac76e6ae045e1a89072df69dec8fc95
Cedricium <amaya.cedric@gmail.com>
2019-08-07 19:32:42 -0700

    Handle GET /api/accounts route & database query

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
-
 const db = require('./data/dbConfig.js');
 
 const server = express();
-
 server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.json({ message: `Hello, world!` })
+})
+
+server.get('/api/accounts', async (req, res) => {
+  try {
+    const accounts = await db('accounts')
+    res.json(accounts)
+  } catch (error) {
+    res.status(500).json({
+      error: `Error occurred while attempting to get the accounts`
+    })
+  }
+})
 
 module.exports = server;
