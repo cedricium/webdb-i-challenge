a521f532022b86e1499d3012f1a259ca70b07119
Cedricium <amaya.cedric@gmail.com>
2019-08-07 19:40:53 -0700

    Handle POST on accounts

--- server.js
+++ server.js
@@ -13,8 +13,24 @@ server.get('/api/accounts', async (req, res) => {
     const accounts = await db('accounts')
     res.json(accounts)
   } catch (error) {
+    console.error(error)
     res.status(500).json({
       error: `Error occurred while attempting to get the accounts`
+    })
+  }
+})
+
+server.post('/api/accounts', async (req, res) => {
+  const accountData = req.body
+  try {
+    const id = (await db('accounts').insert(accountData))[0]
+    const account = await db('accounts').where({ id })
+    console.log(account)
+    res.status(201).json({ ...account })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({
+      error: `Error occurred while attempting to add account`
     })
   }
 })
